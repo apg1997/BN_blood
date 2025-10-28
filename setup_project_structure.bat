bc89a4ee61fe7084779e56e29b561fa459b7c29f
Arkaprabha Gun <apg1997@gmail.com>
2025-10-28 16:47:51 -0400

    batch script to make private folders

---
+++ setup_project_structure.bat
@@ -0,0 +1,18 @@
+
+
+# Create main directories
+mkdir ".git"
+mkdir ".Rproj.user"
+mkdir "1_data"
+mkdir "2_scripts"
+mkdir "3_intermediate"
+mkdir "4_output"
+mkdir "5_manuscript"
+mkdir "Admin"
+
+# Create subdirectories
+mkdir "1_data\private"
+mkdir "3_intermediate\private"
+mkdir "4_output\private"
+
+
